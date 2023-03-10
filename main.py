b971310d402f0cc5546be6703c88e766e7904e9b
JaeHoonKOR <jazzyrain722@naver.com>
2023-03-10 05:44:07 +0000

    NLP_assignment_week1

--- main.py
+++ main.py
@@ -18,9 +18,27 @@ for i in range(0, len(a_list), step):
     end = i+step
     print('{}'.format(a_list[start:end]))
     start = end
+  
     
 '''
 다음 문제를 연습해보시기 바랍니다.
 리스트 안에서 원하는 값들만 뽑아서 리스트를 다시 만들어 보는것을 연습해보기 위해 문제를 제출합니다.
 리스트 안의 값 중 30 이상이고 60 이하인 숫자들의 갯수와 모두 더한 평균을 출력해보세요.
 '''
+count = 0
+for num in a_list:
+    if 30 <= num <= 60:
+        count += 1
+
+print("30 이상이고 60 이하인 숫자의 갯수: {}".format(count))
+
+total = 0
+for num in a_list:
+    if 30 <= num <= 60:
+        total += num
+
+if count > 0:
+    average = total / count
+    print("30 이상이고 60 이하인 숫자들의 평균: {:.2f}".format(average))
+else:
+    print("30 이상이고 60 이하인 숫자가 없습니다.")
